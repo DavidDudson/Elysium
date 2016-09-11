2b069b8e8d92fd1016cdce09cd7e75eddefd3346
David Dudson <davidjohndudson@gmail.com>
2016-09-11 18:22:34 +1200

    Codacy: Ensure public methods have return type

--- core_test/src/main/scala/nz/daved/elysium/core/OperatorTestObject.scala
+++ core_test/src/main/scala/nz/daved/elysium/core/OperatorTestObject.scala
@@ -5,12 +5,12 @@ import scala.collection.mutable.ArrayBuffer
 case class OperatorTestObject(var numbers: ArrayBuffer[Int] = new ArrayBuffer()) {
 
   @Operator("+|")
-  def prepend(i: Int) = numbers.prepend(i)
+  def prepend(i: Int): Unit = numbers.prepend(i)
 
   @Operator("|+")
-  def append(i: Int) = numbers.append(i)
+  def append(i: Int): Unit = numbers.append(i)
 
   @Operator("++")
-  def combine(is: Seq[Int]) = numbers ++= is
+  def combine(is: Seq[Int]): Unit = numbers ++= is
 
 }
