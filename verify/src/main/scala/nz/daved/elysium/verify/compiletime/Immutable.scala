72abcf3d7cd1525f13a663125432def5374a9497
David Dudson <davidjohndudson@gmail.com>
2016-09-26 22:16:46 +1300

    Add scoverage to travis

---
+++ verify/src/main/scala/nz/daved/elysium/verify/compiletime/Immutable.scala
@@ -0,0 +1,11 @@
+package nz.daved.elysium.verify.compiletime
+
+/**
+  * This is a close approximation to guarenteeing immutabilty.
+  *
+  * At present this can only detect mutable fields and mutable collections
+  *
+  */
+class Immutable {
+
+}
