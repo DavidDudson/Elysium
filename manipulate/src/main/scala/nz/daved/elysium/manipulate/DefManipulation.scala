01028cf7c92f5a8377799d73ec897d88c59c407d
David Dudson <davidjohndudson@gmail.com>
2016-09-26 21:20:40 +1300

    Replace Quasiquotes with case class amtching

--- manipulate/src/main/scala/nz/daved/elysium/manipulate/DefManipulation.scala
+++ manipulate/src/main/scala/nz/daved/elysium/manipulate/DefManipulation.scala
@@ -8,44 +8,32 @@ trait DefManipulation {
     def appendStat(stat: String): Defn.Def = appendStat(stat.parse[Stat].get)
     def appendStat(stat: Stat): Defn.Def = {
       defn match {
-        case q"..$mods def $name[..$tparams](...$paramss): $tpeopt = { ..${stats: immutable.Seq[Stat]} }" =>
-          q"..$mods def $name[..$tparams](...$paramss): $tpeopt = { ..$stats; $stat}".asInstanceOf[Defn.Def]
-        case q"..$mods def $name[..$tparams](...$paramss): $tpeopt = $expr" =>
-          q"..$mods def $name[..$tparams](...$paramss): $tpeopt = { $expr; $stat }".asInstanceOf[Defn.Def]
+        case Defn.Def(_,_,_,_,_, Term.Block(stats)) =>
+          defn.copy(body = Term.Block(stats :+ stat))
+        case Defn.Def(_,_,_,_,_, singleStat) =>
+          defn.copy(body = Term.Block(singleStat :: stat :: Nil))
       }
     }
 
     def prependStat(stat: String): Defn.Def = prependStat(stat.parse[Stat].get)
     def prependStat(stat: Stat): Defn.Def = {
       defn match {
-        case q"..$mods def $name[..$tparams](...$paramss): $tpeopt = { ..${stats: immutable.Seq[Stat]} }" =>
-          q"..$mods def $name[..$tparams](...$paramss): $tpeopt = { $stat ; ..$stats}".asInstanceOf[Defn.Def]
-        case q"..$mods def $name[..$tparams](...$paramss): $tpeopt = $expr" =>
-          q"..$mods def $name[..$tparams](...$paramss): $tpeopt = { $stat ; $expr }".asInstanceOf[Defn.Def]
+        case Defn.Def(_,_,_,_,_, Term.Block(stats)) =>
+          defn.copy(body = Term.Block(stat +: stats))
+        case Defn.Def(_,_,_,_,_, singleStat) =>
+          defn.copy(body = Term.Block(stat :: singleStat :: Nil))
       }
     }
 
-    def replaceStats(stats: immutable.Seq[Stat]): Defn.Def = {
-      defn match {
-        case q"..$mods def $name[..$tparams](...$paramss): $tpeopt = $_" =>
-          q"..$mods def $name[..$tparams](...$paramss): $tpeopt = { ..$stats }".asInstanceOf[Defn.Def]
-      }
-    }
+    def replaceStats(stats: immutable.Seq[Stat]): Defn.Def =
+      defn.copy(body = Term.Block(stats))
 
-    def deleteStats: Defn.Def = replaceStats(immutable.Seq())
+    def deleteStats: Defn.Def = defn.copy(body = Term.Block(Nil))
+
 
     // TODO: Make HasName a typeclass and use that instead
-    def rename(name: Term.Name): Defn.Def = {
-      val result: Tree = defn match {
-        case q"..$mods def $_[..$tparam](...$params):$treturn = { ..$stats }" =>
-          q"..$mods def $name[..$tparam](...$params):$treturn = { ..$stats }"
-        case q"..$mods def $_[..$tparam](...$params):$treturn = $expr" =>
-          q"..$mods def $name[..$tparam](...$params):$treturn = $expr"
-      }
-
-      //TODO: Replace with some same casting method that gives decent error messages
-      result.asInstanceOf[Defn.Def]
-    }
+    def rename(name: Term.Name): Defn.Def =
+      defn.copy(name = name)
   }
 }
 
