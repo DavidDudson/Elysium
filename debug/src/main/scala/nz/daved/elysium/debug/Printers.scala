2ff009c0ce917c442210b1323681b25d10009dc0
David Dudson <davidjohndudson@gmail.com>
2016-09-26 20:48:28 +1300

    Add compileTimeOnly annotations to printers

--- debug/src/main/scala/nz/daved/elysium/debug/Printers.scala
+++ debug/src/main/scala/nz/daved/elysium/debug/Printers.scala
@@ -2,9 +2,10 @@ package nz.daved.elysium.debug
 
 import pprint._
 
-import scala.annotation.StaticAnnotation
+import scala.annotation.{StaticAnnotation, compileTimeOnly}
 import scala.meta._
 
+@compileTimeOnly("@printStructure not expanded")
 class printStructure extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
     pprintln(defn.show[Structure])
@@ -12,6 +13,7 @@ class printStructure extends StaticAnnotation {
   }
 }
 
+@compileTimeOnly("@printSyntax not expanded")
 class printSyntax extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
     pprintln(defn.show[Syntax])
